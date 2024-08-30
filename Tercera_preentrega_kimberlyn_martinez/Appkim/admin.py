fd495c45a6ee20cc0871fd241a24ca878d6602e2
yisel35 <kimberlynyisel35@gmail.com>
2024-08-29 23:08:41 -0300

    Delete Tercera_preentrega_kimberlyn_martinez directory

--- Tercera_preentrega_kimberlyn_martinez/Appkim/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Appkim.models import Curso, Estudiante, Profesor
-# Register your models here.
-admin.site.register(Curso)
-admin.site.register(Estudiante)
-admin.site.register(Profesor)
--- Tercera_preentrega_kimberlyn_martinez/Appkim/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppkimConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Appkim'
